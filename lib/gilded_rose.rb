085cc38ab1648a7e8eb2277482a45b27eae74aef
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-02-03 16:26:14 +0000

    added quality checks for quality not eing above 50 or below 0 at the start

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,50 +8,47 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-    #check for everything thats not Aged brie, pass or sulfuras --> lowers quality
-    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros"
-      if item.quality > 0 
+    if item.quality < 50 && item.quality > 0
+      #check for everything thats not Aged brie, pass or sulfuras --> lowers quality
+      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros"
         item.quality = item.quality - 1
-      end
 
-      # raises value of brie 
-    elsif item.name == "Aged Brie"
-      if item.quality < 50
+        # raises value of brie 
+      elsif item.name == "Aged Brie"
         item.quality = item.quality + 1
-      end
 
-      # update quality of backstage pass
-    elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-      if item.quality < 50
+        # update quality of backstage pass
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
         item.quality = item.quality + 1
+ 
+        if item.sell_in < 11
+          item.quality = item.quality + 1 if item.quality < 50
+        end
+        if item.sell_in < 6
+          item.quality = item.quality + 1 if item.quality < 50
+        end
       end
-      if item.sell_in < 11
-        item.quality = item.quality + 1 if item.quality < 50
-      end
-      if item.sell_in < 6
-        item.quality = item.quality + 1 if item.quality < 50
-      end
-    end
 
-    # decreases sell_in for all but Sulfuras 
-    update_sell_in(item)
+      # decreases sell_in for all but Sulfuras 
+      update_sell_in(item)
 
-    # sell in below 0 and quality not bellow 0
-    if item.sell_in < 0 && item.quality > 0
-      if item.name != "Aged Brie"
-        if item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros"
-          item.quality = item.quality - 1
+      # sell in below 0 and quality not bellow 0
+      if item.sell_in < 0 && item.quality > 0
+        if item.name != "Aged Brie"
+          if item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros"
+            item.quality = item.quality - 1
+          else
+            # backstage pass quality becomes 0 after sell_in date reaches 0
+            item.quality = item.quality - item.quality
+          end
         else
-          # backstage pass quality becomes 0 after sell_in date reaches 0
-          item.quality = item.quality - item.quality
-        end
-
-      else
-        # lowers brie again
-        if item.quality < 50
-          item.quality = item.quality + 1
+          # lowers brie again
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
         end
       end
+
     end
   end
   end
