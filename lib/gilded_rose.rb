7d631415f452a19d91b21404d985a740dd470462
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-02-03 15:53:28 +0000

    splitting checks for brie and backstage pass

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,37 +9,32 @@ class GildedRose
   def update_quality()
     @items.each do |item|
     #check for everything thats not Aged brie, pass or sulfuras --> lowers quality
-    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-      # if its not sulfuras --> lower it
-      if item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros"
+    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros"
+      if item.quality > 0 
         item.quality = item.quality - 1
       end
 
-    else
+      # raises value of brie 
+    elsif item.name == "Aged Brie"
       if item.quality < 50
-        # raises value of brie and backstage
         item.quality = item.quality + 1
+      end
 
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if item.sell_in < 11
-            item.quality = item.quality + 1 if item.quality < 50
-          end
-
-          if item.sell_in < 6
-            item.quality = item.quality + 1 if item.quality < 50
-          end
-        end
+      # update quality of backstage pass
+    elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+      if item.quality < 50
+        item.quality = item.quality + 1
+      end
+      if item.sell_in < 11
+        item.quality = item.quality + 1 if item.quality < 50
+      end
+      if item.sell_in < 6
+        item.quality = item.quality + 1 if item.quality < 50
       end
     end
-    update_sell_in(item)
-  end
-  end
 
-  private 
-
-  def update_sell_in(item)
     # decreases sell_in for all but Sulfuras 
-    item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
+    update_sell_in(item)
 
     # sell in below 0 and quality not bellow 0
     if item.sell_in < 0 && item.quality > 0
@@ -59,5 +54,11 @@ class GildedRose
       end
     end
   end
+  end
 
+  private 
+
+  def update_sell_in(item)
+    item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
+  end
 end
