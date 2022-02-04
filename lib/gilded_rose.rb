24d175d378f4b2af9088d19bbcee29167a2e4a03
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-02-04 12:03:40 +0000

    removed initial if statement and created class to increase and decrease quality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,36 +9,49 @@ class GildedRose
   end
 
   def update_quality
+    
     @items.each do |item|
-      if valid_item_quality?(item)
-        # check for not a special item
-        if not_special_item?(item)
-          item.quality -= expired?(item) && valid_item_quality?(item) ? 2 : 1
+      # check for not a special item
+      if not_special_item?(item)
+        decrease_value(item)
+        decrease_value(item) if expired?(item)
 
-        elsif item.name == 'Aged Brie'
-          item.quality += expired?(item) ? 2 : 1
+      elsif item.name == 'Aged Brie'
+        increase_value(item)
+        increase_value(item) if expired?(item)
 
-        # update quality of backstage pass
-        elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if expired?(item) && item.quality > MIN_QUALITY
-            update_sell_in(item)
-            return item.quality = 0
-          end
-          item.quality = item.quality + 1
-
-          item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < MAX_QUALITY)
-          item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < MAX_QUALITY)
+      # update quality of backstage pass
+      elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if expired?(item) && valid_item_quality?(item)
+          update_sell_in(item)
+          return item.quality = 0
         end
+        if item.sell_in < 11 && (item.quality < MAX_QUALITY)
+          item.quality += 1
+          if item.sell_in < 6 && (item.quality < MAX_QUALITY)
+            item.quality += 1
+          end
+        end
+
+        item.quality +=1
       end
       # decreases sell_in for all but Sulfuras
-      update_sell_in(item)
+      update_sell_in(item) unless item.name == 'Sulfuras, Hand of Ragnaros'
     end
   end
 
   private
 
+  def decrease_value(item)
+    item.quality -= 1 if item.quality > MIN_QUALITY
+  end
+
+  def increase_value(item)
+    item.quality += 1 if item.quality < MAX_QUALITY
+  end
+
   def update_sell_in(item)
-    item.sell_in -= 1 unless item.name == 'Sulfuras, Hand of Ragnaros'
+    item.sell_in -= 1 
   end
 
   def valid_item_quality?(item)
